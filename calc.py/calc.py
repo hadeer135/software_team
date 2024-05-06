1f63bea357695252905aa0ffcccefbd1f67401a7
hadeer135 <154346647+hadeer135@users.noreply.github.com>
2024-05-06 23:01:24 +0300

    Update calc.py
    " Added Factorial functions"

--- calc.py/calc.py
+++ calc.py/calc.py
@@ -23,3 +23,16 @@ def button_delete():
     text = calc_operator[:-1]
     calc_operator = text
     text_input.set(text)
+
+# Function to calculate the factorial of a number
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+def fact_func():
+    global calc_operator
+    result = str(factorial(int(calc_operator)))
+    calc_operator = result
+    text_input.set(result
