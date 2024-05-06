501c6a15db7f1aa9d6dde1df7d2b12c9559a1f77
MagdyElhefny <154082438+MagdyElhefny@users.noreply.github.com>
2024-05-06 19:53:59 +0200

    Add files via upload

---
+++ calc.py/calc.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import math
+
+
+'''
+Functions
+'''
+# Function to add in the entry of text display
+def button_click(char):
+    global calc_operator
+    calc_operator += str(char)
+    text_input.set(calc_operator)
+
+# Function to clear the whole entry of text display
+def button_clear_all():
+    global calc_operator
+    calc_operator = ""
+    text_input.set("")
+
+# Function to delete one by one from the last in the entry of text display
+def button_delete():
+    global calc_operator
+    text = calc_operator[:-1]
+    calc_operator = text
+    text_input.set(text)
